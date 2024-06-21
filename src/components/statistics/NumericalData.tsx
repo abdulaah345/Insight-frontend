771f87820fdcdf8e4fcf5c1816ae2cf3b24e5703
abdullah345 <abdallahmohamed54655@gmail.com>
2024-06-21 20:03:55 +0300

    v1

--- src/components/statistics/NumericalData.tsx
+++ src/components/statistics/NumericalData.tsx
@@ -1,52 +1,173 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// interface DataEntry {
+//   [key: string]: string;
+// }
+
+// const data: DataEntry[] = [
+//   {
+//     id: "1",
+//     CustomerId: "DD37Cf93aecA6Dc",
+//     FirstName: "Sheryl",
+//     LastName: "Baxter",
+//     Company: "Rasmussen Group",
+//     City: "East Leonard",
+//     Country: "Chile",
+//     Phone1: "6",
+//     Phone2: "8",
+//     Email: "zunigavanessa@smith.info",
+//     SubscriptionDate: "2020-08-24",
+//     Website: "http://www.stephenson.com/",
+//   },
+//   {
+//     id: "86",
+//     CustomerId: "C6763c99d0bd16D",
+//     FirstName: "Emma",
+//     LastName: "Cunningham",
+//     Company: "Stephens Inc",
+//     City: "North Jillianview",
+//     Country: "New Zealand",
+//     Phone1: "2",
+//     Phone2: "4",
+//     Email: "walter83@juarez.org",
+//     SubscriptionDate: "2022-05-13",
+//     Website: "http://www.reid.info/",
+//   },
+// ];
+
+// const NumericalData = () => {
+//   const numericalColumns = Object.keys(data[0]).filter((columnName) =>
+//     data.every((entry) => !isNaN(parseFloat(entry[columnName])))
+//   );
+
+//   const checkDataType = (data: any[]) => {
+//     const isNumerical = data.every((value: number) => !isNaN(value));
+//     return isNumerical ? "Numerical" : "Categorical";
+//   };
+
+//   const extractNumericalValues = (columnName: string) => {
+//     return data.map((entry) => parseFloat(entry[columnName]) || 0);
+//   };
+
+//   const columnData = numericalColumns.map((columnName) =>
+//     extractNumericalValues(columnName)
+//   );
+
+//   return (
+//     <div className="container mt-5">
+//       <h2 className="mb-4">Data Statistics</h2>
+//       <div className="table-responsive">
+//         <table className="table table-bordered">
+//           <thead className="thead-dark">
+//             <tr>
+//               <th></th>
+//               {numericalColumns.map((columnName, index) => (
+//                 <th key={index}>{columnName}</th>
+//               ))}
+//             </tr>
+//           </thead>
+//           <tbody>
+//             <tr>
+//               <td>Mean</td>
+//               {columnData.map((data, index) => (
+//                 <td key={index}>
+//                   {(
+//                     data.reduce((acc, val) => acc + val, 0) / data.length
+//                   ).toFixed(2)}
+//                 </td>
+//               ))}
+//             </tr>
+//             <tr>
+//               <td>Median</td>
+//               {columnData.map((data, index) => (
+//                 <td key={index}>{calculateMedian(data)}</td>
+//               ))}
+//             </tr>
+//             <tr>
+//               <td>Minimum</td>
+//               {columnData.map((data, index) => (
+//                 <td key={index}>{Math.min(...data)}</td>
+//               ))}
+//             </tr>
+//             <tr>
+//               <td>Maximum</td>
+//               {columnData.map((data, index) => (
+//                 <td key={index}>{Math.max(...data)}</td>
+//               ))}
+//             </tr>
+//             <tr>
+//               <td>Standard Deviation</td>
+//               {columnData.map((data, index) => (
+//                 <td key={index}>
+//                   {calculateStandardDeviation(data).toFixed(2)}
+//                 </td>
+//               ))}
+//             </tr>
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const calculateMedian = (data: any[]) => {
+//   const sortedData = data.slice().sort((a, b) => a - b);
+//   const middleIndex = Math.floor(sortedData.length / 2);
+//   return sortedData.length % 2 === 0
+//     ? (sortedData[middleIndex - 1] + sortedData[middleIndex]) / 2
+//     : sortedData[middleIndex];
+// };
+
+// const calculateStandardDeviation = (data: any[]) => {
+//   const mean = data.reduce((acc, val) => acc + val, 0) / data.length;
+//   return Math.sqrt(
+//     data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / data.length
+//   );
+// };
+
+// export default NumericalData;
+
+
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 interface DataEntry {
-  [key: string]: string;
+  [key: string]: string | number;
 }
 
-const data: DataEntry[] = [
-  {
-    id: "1",
-    CustomerId: "DD37Cf93aecA6Dc",
-    FirstName: "Sheryl",
-    LastName: "Baxter",
-    Company: "Rasmussen Group",
-    City: "East Leonard",
-    Country: "Chile",
-    Phone1: "6",
-    Phone2: "8",
-    Email: "zunigavanessa@smith.info",
-    SubscriptionDate: "2020-08-24",
-    Website: "http://www.stephenson.com/",
-  },
-  {
-    id: "86",
-    CustomerId: "C6763c99d0bd16D",
-    FirstName: "Emma",
-    LastName: "Cunningham",
-    Company: "Stephens Inc",
-    City: "North Jillianview",
-    Country: "New Zealand",
-    Phone1: "2",
-    Phone2: "4",
-    Email: "walter83@juarez.org",
-    SubscriptionDate: "2022-05-13",
-    Website: "http://www.reid.info/",
-  },
-];
+interface Props {
+  data: DataEntry[];
+}
 
-const NumericalData = () => {
+const NumericalData: React.FC<Props> = ({ data }) => {
+  if (data.length === 0) {
+    return <div>No data available</div>;
+  }
+
   const numericalColumns = Object.keys(data[0]).filter((columnName) =>
-    data.every((entry) => !isNaN(parseFloat(entry[columnName])))
+    data.every((entry) => !isNaN(parseFloat(entry[columnName] as string)))
   );
 
-  const checkDataType = (data: any[]) => {
-    const isNumerical = data.every((value: number) => !isNaN(value));
-    return isNumerical ? "Numerical" : "Categorical";
+  const extractNumericalValues = (columnName: string) => {
+    return data.map((entry) => parseFloat(entry[columnName] as string) || 0);
   };
 
-  const extractNumericalValues = (columnName: string) => {
-    return data.map((entry) => parseFloat(entry[columnName]) || 0);
+  const calculateMean = (data: number[]) =>
+    data.reduce((acc, val) => acc + val, 0) / data.length;
+
+  const calculateMedian = (data: number[]) => {
+    const sortedData = data.slice().sort((a, b) => a - b);
+    const middleIndex = Math.floor(sortedData.length / 2);
+    return sortedData.length % 2 === 0
+      ? (sortedData[middleIndex - 1] + sortedData[middleIndex]) / 2
+      : sortedData[middleIndex];
+  };
+
+  const calculateStandardDeviation = (data: number[]) => {
+    const mean = calculateMean(data);
+    return Math.sqrt(
+      data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / data.length
+    );
   };
 
   const columnData = numericalColumns.map((columnName) =>
@@ -70,11 +191,7 @@ const NumericalData = () => {
             <tr>
               <td>Mean</td>
               {columnData.map((data, index) => (
-                <td key={index}>
-                  {(
-                    data.reduce((acc, val) => acc + val, 0) / data.length
-                  ).toFixed(2)}
-                </td>
+                <td key={index}>{calculateMean(data).toFixed(2)}</td>
               ))}
             </tr>
             <tr>
@@ -98,30 +215,13 @@ const NumericalData = () => {
             <tr>
               <td>Standard Deviation</td>
               {columnData.map((data, index) => (
-                <td key={index}>
-                  {calculateStandardDeviation(data).toFixed(2)}
-                </td>
+                <td key={index}>{calculateStandardDeviation(data).toFixed(2)}</td>
               ))}
             </tr>
           </tbody>
         </table>
       </div>
     </div>
-  );
-};
-
-const calculateMedian = (data: any[]) => {
-  const sortedData = data.slice().sort((a, b) => a - b);
-  const middleIndex = Math.floor(sortedData.length / 2);
-  return sortedData.length % 2 === 0
-    ? (sortedData[middleIndex - 1] + sortedData[middleIndex]) / 2
-    : sortedData[middleIndex];
-};
-
-const calculateStandardDeviation = (data: any[]) => {
-  const mean = data.reduce((acc, val) => acc + val, 0) / data.length;
-  return Math.sqrt(
-    data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / data.length
   );
 };
 
